33931ae61a9c3ce659aa95d08e7b738aaf7157f6
Alex Nypels <nypels.alexandre@outlook.com>
2022-04-23 14:45:17 +0100

    Adding style to order page

--- order/static/order/js/index.js
+++ order/static/order/js/index.js
@@ -3,3 +3,22 @@ $('#category_select_input').on('change', (e) => {
     let value = $('#category_select_input').val()
     window.location = "?category=" + value;
 })
+
+
+//change border color on item when ser hover it
+$('.cocktail-name').on('mouseover', (e) => {
+    let parent = $(e.target).parent()
+    let grand_parent = $(parent).parent()
+
+    $(grand_parent).css({'border-color': '#C27803'})
+    $(parent).css('color', '#C27803')
+})
+
+//change border color on item when ser hover it
+$('.cocktail-name').on('mouseleave', (e) => {
+    let parent = $(e.target).parent()
+    let grand_parent = $(parent).parent()
+
+    $(grand_parent).css('border-color', 'white')
+    $(parent).css('color', 'white')
+})

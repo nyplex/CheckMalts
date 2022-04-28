2f945ad89775585b3249a12fc43979373d48eb92
Alex Nypels <nypels.alexandre@outlook.com>
2022-04-28 12:13:29 +0100

    Adding more tests on order app & adding 404 template

--- menu/tests.py
+++ menu/tests.py
@@ -16,28 +16,11 @@ class MenuView(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'menu/menu.html')
         self.assertContains(
-            response, '<div class="h-[100vh] w-full relative overflow-x-hidden" id="menuHeaderContainer">')
+            response, '<div class="h-[100vh] w-full relative overflow-x-hidden bg-contrastColor py-4" id="menuHeaderContainer">')
         self.assertIn('cocktails', response.context)
         self.assertIn('categories', response.context)
         self.assertIn('subCategories', response.context)
     
-
-    def test_product_modal(self):
-        client = Client()
-
-        cocktail = Cocktail.objects.create(
-            name='Test cocktail', price=10, prep_time=1, description='Lorem Lispum Test description')
-        cocktail.save()
-        
-        response = client.get('/menu/1')
-        messages = list(response.context['messages'])
-        
-        self.assertEquals(response.status_code, 200)
-        self.assertEqual(Cocktail.objects.count(), 1)
-        self.assertEqual(str(messages[0].message.price), '10.0')
-        self.assertEqual(str(messages[0].message.name), 'Test cocktail')
-        self.assertContains(response, '<div id="productModal" tabindex="-1"')
-
 
 class NavbarView(TestCase):
     def test_navbar_logged_out(self):

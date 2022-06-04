8a29291f5414ef2499d7f3cb11b93950e529b2a7
Alex Nypels <nypels.alexandre@outlook.com>
2022-06-04 15:07:52 +0100

    Adding staticfiles folder

--- CheckMalts/settings.py
+++ CheckMalts/settings.py
@@ -150,6 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type

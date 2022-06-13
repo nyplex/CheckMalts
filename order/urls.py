36ff29ccfae44ba504805bf22717a38696abcf78
Alex Nypels <nypels.alexandre@outlook.com>
2022-06-13 19:45:22 +0100

    Fixing order-again url

--- order/urls.py
+++ order/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.order, name='order'),
     path('/item-detail', views.item_detail, name='item-detail'),
     path('/size-price', views.calculate_size_price, name='size-price'),
-    path('order-again/<order_id>', views.order_again, name='order_again')
+    path('/order-again/<order_id>', views.order_again, name='order_again')
 ]
